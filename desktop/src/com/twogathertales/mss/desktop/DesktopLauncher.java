0bc16673657f81db7311af49de05a4164a0bab20
JoaoGSRocha <joaogsrocha@gmail.com>
2019-04-20 18:02:20 +0100

    [Init] Upload of Initial Assets & Setup
    Notes: Setup JDK according your own machines.
    
    Keywords: Init, Upload, Assets, Setup, Settings
    Importance: Critical

---
+++ desktop/src/com/twogathertales/mss/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.twogathertales.mss.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.twogathertales.mss.MasterOfSwordsStudio;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MasterOfSwordsStudio(), config);
+	}
+}
