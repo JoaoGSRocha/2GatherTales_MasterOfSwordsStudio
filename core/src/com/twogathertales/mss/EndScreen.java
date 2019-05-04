80c28dde7d45d342dc55debac80d2ff04b12467d
Tiago Cabral <riskybomberpt@gmail.com>
2019-05-04 04:37:30 +0100

    [Add] Credits

--- core/src/com/twogathertales/mss/EndScreen.java
+++ core/src/com/twogathertales/mss/EndScreen.java
@@ -17,6 +17,8 @@ public class EndScreen {
     private BitmapFont bitmapFontVictory;
     private BitmapFont bitmapFontDefeat;
     private BitmapFont bitmapFont;
+    private BitmapFont bitmapFontCredits;
+    private BitmapFont bitmapFontCreditsMusic;
 
     public void create(){
         FreeTypeFontGenerator generatorExtrabold = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-ExtraBold.ttf"));
@@ -33,10 +35,17 @@ public class EndScreen {
         parameter.borderWidth = 2;
         parameter.color = Color.BLACK;
         bitmapFont = generatorSemibold.generateFont(parameter);
+
+        FreeTypeFontParameter parameterCredits = new FreeTypeFontParameter();
+        parameterCredits.size = 18;
+        parameterCredits.shadowOffsetX = parameter.shadowOffsetY = 1;
+        parameterCredits.borderWidth = 1;
+        bitmapFontCredits = generatorSemibold.generateFont(parameterCredits);
+        parameterCredits.size = 12;
+        bitmapFontCreditsMusic = generatorSemibold.generateFont(parameterCredits);
+
         generatorExtrabold.dispose();
         generatorSemibold.dispose();
-
-
 
     }
 
@@ -48,5 +57,23 @@ public class EndScreen {
             bitmapFontDefeat.draw(batch,"Defeat",20,400);
         }
         bitmapFont.draw(batch,"Points: "+ MapManager.points,40,320);
+
+        bitmapFontCredits.draw(batch,"Credits: ",40,240);
+        bitmapFontCredits.draw(batch,"Lead Developer:",40,200);
+        bitmapFontCredits.draw(batch,"Tiago \"Risky\" Cabral",40,180);
+
+        bitmapFontCredits.draw(batch,"Developers:",40,140);
+        bitmapFontCredits.draw(batch,"João Rocha",40,120);
+        bitmapFontCredits.draw(batch,"Alexandre Nunes",40,100);
+
+        bitmapFontCredits.draw(batch,"Artist:",300,200);
+        bitmapFontCredits.draw(batch,"Érica Monteiro",300,180);
+
+        bitmapFontCreditsMusic.draw(batch,"Music from https://filmmusic.io/:",260,140);
+        bitmapFontCreditsMusic.draw(batch,"\"Achaidh Cheide\"",260,122);
+        bitmapFontCreditsMusic.draw(batch,"by Kevin MacLeod (https://incompetech.com/)",260,104);
+        bitmapFontCreditsMusic.draw(batch,"Licence:",260,86);
+        bitmapFontCreditsMusic.draw(batch,"CC BY (http://creativecommons.org/licenses/by/4.0/)",260,68);
+
     }
 }
