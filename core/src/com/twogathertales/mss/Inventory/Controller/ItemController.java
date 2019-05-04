7f14fcd5d11a8c394a8da7dad154319fd840363d
Tiago Cabral <riskybomberpt@gmail.com>
2019-05-04 03:09:28 +0100

    [Add] More text!
    Labels for inventories
    Text for loot obtained

--- core/src/com/twogathertales/mss/Inventory/Controller/ItemController.java
+++ core/src/com/twogathertales/mss/Inventory/Controller/ItemController.java
@@ -1,9 +1,14 @@
 package com.twogathertales.mss.Inventory.Controller;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.twogathertales.mss.Components.MapManager;
 import com.twogathertales.mss.Inventory.Model.Item;
 import com.twogathertales.mss.Inventory.Model.ItemsCity;
@@ -12,22 +17,35 @@ import com.twogathertales.mss.Inventory.Model.ItemsPlayer;
 import java.util.ArrayList;
 
 public class ItemController {
+    private BitmapFont bitmapFont;
     private MapManager map;
+    private boolean drawLoot = false;
+    private String[] lootToDraw;
+    private long startTimeMs;
     final static int WIN_HEIGHT = 480;
     float timeSinceClicking = 0;
     public static ItemsCity itemsCity = new ItemsCity();
     public static ItemsPlayer itemsPlayer = new ItemsPlayer();
-    public static ArrayList<Item> itemAL = new ArrayList<Item>();
     public void create(){
         map = new MapManager();
-        itemAL.addAll(itemsCity.getItems());
-        itemAL.addAll(itemsPlayer.getItems());
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-SemiBold.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 14;
+        parameter.color = new Color(100f/255f, 160f/255f, 20f/255f, 1);
+        parameter.shadowOffsetX = parameter.shadowOffsetY = 2;
+        parameter.borderWidth = 2;
+        bitmapFont = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     public void render(Batch batch){
         renderPlayerItems(batch);
         renderCityItems(batch);
         sellItems();
+        if(drawLoot) {
+          showLootDraw(batch);
+        }
     }
 
     /***
@@ -92,6 +110,27 @@ public class ItemController {
                     item.setQuantity(++currQuant);
                     System.out.println("Adding one " + item.getName() + " " + item.getQuantity());
                 }
+        }
+        showLoot(items);
+    }
+
+    public void showLoot(String[] items){
+        lootToDraw = items;
+        drawLoot = true;
+        startTimeMs = TimeUtils.millis();
+    }
+
+    public void showLootDraw(Batch batch){
+        bitmapFont.draw(batch,"Quest completed!",190,280);
+        bitmapFont.draw(batch,"You obtained:",190,264);
+        int y = 248;
+        for(String s : lootToDraw){
+            bitmapFont.draw(batch,"1x "+s,190,y);
+            y-=16;
+        }
+
+        if(TimeUtils.millis()-startTimeMs>2500){
+            drawLoot = false;
         }
     }
 
--- core/src/com/twogathertales/mss/Inventory/Inventory.java
+++ core/src/com/twogathertales/mss/Inventory/Inventory.java
@@ -1,24 +1,38 @@
 package com.twogathertales.mss.Inventory;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.twogathertales.mss.Inventory.Controller.ItemController;
 
 public class Inventory {
 
     private Texture inv_grid;
     public static ItemController itemController;
+    private BitmapFont bitmapFont;
 
     public void create () {
         itemController = new ItemController();
-        //inv_grid = new Texture("Backg_Grid_Vert.jpg"); //160 265
         inv_grid = new Texture("bg_full.png");
         itemController.create();
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-SemiBold.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 18;
+        parameter.color = new Color(220f/255f, 160f/255f, 50f/255f, 1);
+        parameter.shadowOffsetX = parameter.shadowOffsetY = 2;
+        parameter.borderWidth = 2;
+        bitmapFont = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     public void render (Batch batch) {
         batch.draw(inv_grid, 0, 0);
-        //batch.draw(inv_grid, 480, 0);
+        bitmapFont.draw(batch,"Inventory",24,300);
+        bitmapFont.draw(batch,"City",530,300);
         itemController.render(batch);
     }
 
